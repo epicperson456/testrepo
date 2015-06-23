abb42d837f763d9cd89bc9485aabbfd063b769f2
epicperson456 <epicperson456@gmail.com>
2015-06-22 20:10:14 -0400

    asdf

---
+++ tb100000000000000.java
@@ -0,0 +1,38 @@
+package thenidied;
+
+public class tb100000000000000 {
+
+	public static void main(String[] args) {
+		
+		boolean enrolledbool = true;
+		int grade = 1337;
+		if (enrolledbool == true && grade <= 1336 && grade >= 90)
+		{
+			System.out.println("hey m8 gg u got an @"); //<---- = epic.message
+		}
+		
+		if (enrolledbool == true && grade <= 90 && grade >= 80)
+		{
+			System.out.println("omg gg m8 u got teh 80 - 0 = 8 m8 no h8 u got BbBbBbBbB");
+		}
+		if (enrolledbool == true && grade <= 80 && grade >= 70)
+		{
+			System.out.println("w8, did u c dat. u got dat c");
+		}
+		if (enrolledbool == true && grade <=70 && grade >=60)
+		{
+			System.out.println("l0o0o0o0o0ol u just got dat D :--((");
+		}
+		
+		if (enrolledbool == true && grade <= 60)
+		{
+			System.out.println("f :(");
+		}
+		if (enrolledbool == true && grade == 1337)
+		{
+			System.out.println("OGMGMOGOMGOMGMOOGMOM U = EPIC DAT SWWWAFG $$$WAGG L000000l U NOSCOPED DAT T3ST");
+		}
+		}
+	}
+
+
